d2800d800197380d1baf1840769265a3385f0769
Priya Kasimbeg <kasimbeg@google.com>
2023-04-16 04:21:01 +0000

    fix

--- submission_runner.py
+++ submission_runner.py
@@ -379,7 +379,7 @@ def train_once(
 
           
           # Add times to eval results for logging
-          latest_eval_result['score'] = (train_state['acccumulated_submission_time'])
+          latest_eval_result['score'] = (train_state['accumulated_submission_time'])
           latest_eval_result['total_duration'] = train_state['last_eval_time'] - global_start_time
           latest_eval_result['accumulated_submission_time'] = train_state['accumulated_submission_time']
           
